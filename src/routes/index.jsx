98da82fc9968e29b99f4f59994c3f7443147fc70
Elita1Arcee <louisa@louisajones.dev>
2023-07-11 13:46:24 -0400

    added search spinner for ux

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,13 @@
 export default function Index() {
   return (
-    <p id="zero-state">
-      This is a demo for React Router.
-      <br />
-      Check out{" "}
-      <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
-    </p>
+    <>
+      <h1 className="mainTitle">Bots-N-Cats Pals</h1>
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
+      </p>
+    </>
   );
 }
